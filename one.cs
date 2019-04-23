8b5ce07763794d7be58f0bcd167b1247bf6d7f3b
nicktamburro <nicholastamburro@gmail.com>
2019-04-22 21:30:07 -0700

    comments to break down boilerplate stuff

--- one.cs
+++ one.cs
@@ -10,9 +10,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    //this is our class, called Program
     class Program
     {
         static void Main(string[] args)
+        //like Java:
+        //static means this method should be available without instantiating
+        //the class... like a JS constructor, etc
+        //
+        //void... this method returns NOTHING lebowski
+        //
+        //Main ... this is the entry point
+        //
+        //our Main only takes one argument, "args" of type string
+        //
+        //
         {
             //we use the Console class, which is part of System
             //first line, "using System" makes this available
