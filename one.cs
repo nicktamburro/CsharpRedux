ad8b0ef176499084cd11d30f993fba79002b0149
nicktamburro <nicholastamburro@gmail.com>
2019-04-22 21:24:31 -0700

    namespace and class

--- one.cs
+++ one.cs
@@ -1,3 +1,7 @@
+//the "using" keyword imports a namespace
+//a namespace is a collection of classes
+//
+//these usings are just boilerplate / commonly used ones
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //we use the Console class, which is part of System
+            //first line, "using System" makes this available
             Console.WriteLine("Hello, my dudes.");
             Console.ReadLine();
         }
