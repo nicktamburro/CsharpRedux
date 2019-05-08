1d9587537f4db4627326e623882e404e0b22d6f6
nicktamburro <nicholastamburro@gmail.com>
2019-05-07 22:48:10 -0700

    small edits

--- mvc.cs
+++ mvc.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 //super basic level of our controller
 
-namespace WebApplication1.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ViewResult Index()
-        {
+namespace WebApplication1.Controllers {
+    public class HomeController : Controller {//is HomeController a built-in class?
+
+        //when you return a ViewResult we're asking MVC to render a view
+        public ViewResult Index() {
             return View("MyView");
-        }
-        
+        }   
     }
 }
+//also like in JS, curly brace same line as declaration
+//(it wasn't in the initial stuff I was reading)
